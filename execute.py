87b3ea95fa88f2f84802f4745e3ebac9af2ed39e
auv <ksuauvteam@gmail.com>
2018-07-31 03:51:51 -0400

    more work

--- execute.py
+++ execute.py
@@ -1,1 +1,24 @@
-print('hello world')
+import rospy
+import numpy as np
+
+# box_subscriber 
+
+# create dict of statuses and whether they've been completed
+# that is, track what we've done 
+completed = dict.fromkeys(['start_gate_found', 'start_gate_passed', 'dice_found', 'dice_hit'], False)
+print(completed)
+
+def img_callback():
+
+
+def main():
+    
+    
+    #add killswitch check here
+
+    rospy.spin()
+
+
+
+if __name__ == '__main__'
+    main()
