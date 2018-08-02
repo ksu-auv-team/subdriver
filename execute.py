7527d70317f68aa7bf9f83dd1012dad40cbd9738
auv <ksuauvteam@gmail.com>
2018-08-02 08:59:13 -0400

    bugfixed after testing

--- execute.py
+++ execute.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 import math
 
-use_hold_depth = False
+use_hold_depth = True
 target_depth = 0
 
 # box_callback uses completed to track what's done and states to list the possible states
@@ -31,7 +31,7 @@ class_dict = {"background":0, "path_marker":1, "start_gate":2,
             'roulette_wheel':9, 'red_wheel_side':10, 'black_wheel_side':11,
             'slot_machine':12, 'slot_handle':13, 'r_slot_target':14, 'y_slot_target':15,
             'r_ball_tray':16, 'g_ball_tray':17, 'floating_area':18, 'r_funnel':19,
-            'y_funnel':20, 'g_chip_dispenser':21, 'g_chip_plate':22}
+            'y_funnel':20, 'g_chip_dispenser':21, 'g_chip_plate':22, 'dieX':23}
 
 axes_dict = {'rotate': 0, 'vertical' : 1, 'lt' : 2, 'leftright' : 3, 'frontback' : 4, 'rt' : 5, 'dpad_h' : 6, 'dpad_v' : 7}
 buttons_dict = {'a' : 0, 'b' : 1, 'x' : 2, 'y' : 3, 'lb' : 4, 'rb' : 5, 'back' : 6, 'start' : 7, 'xbox' : 8, 'lstickpress' : 9, 'rstickpress' : 10}
@@ -85,15 +85,17 @@ def get_box_of_class(boxes, class_num):
     found = None
     max_prob = 0.0
     for box in boxes:
-        if box[1] == class_num and box[2] > max_prob:
+        if box[0] == class_num and box[1] > max_prob:
             found = box
-            max_prob = box[2]
+            max_prob = box[1]
+    print('class:\t' + str(box[0]))
 
     #ignore ghosts
     if max_prob > 0.2:
         return found
     else:
         return None
+    
 
 
 # track, move toward, and pass through the gate
@@ -103,6 +105,7 @@ def track(boxes):
     global current_target
     global start_time
     global is_close
+    global target_depth
     current_depth = get_depth
     completed['start_gate_found'] = True #change later, once we have more tasks
     is_close = False
@@ -113,14 +116,14 @@ def track(boxes):
         center = getCenter(box)
         
         if center[0] < .45:
-            msg.axes[axes_dict['leftright']] = 0.4
+            msg.axes[axes_dict['leftright']] = 0.2
         elif center[0] > .55:
-            msg.axes[axes_dict['leftright']] = 0.4
+            msg.axes[axes_dict['leftright']] = -0.2
 
         if center[1] < .45:
-            msg.axes[axes_dict['updown']] = -0.6
+            msg.axes[axes_dict['vertical']] = -0.6
         elif center[1] > .55:
-            msg.axes[axes_dict['updown']] = 0.1
+            msg.axes[axes_dict['vertical']] = 0.1
         else:
             if not use_hold_depth:
                 msg.axes[axes_dict['vertical']] = -.425 #replace with hold_depth later
@@ -138,7 +141,7 @@ def track(boxes):
         target_depth = get_depth()
         start_time = time.time()
         current_state = search_forward
-    pub.publish(msg)
+    return msg
 
 def ramming_speed(boxes):
     global current_state
@@ -147,7 +150,7 @@ def ramming_speed(boxes):
     global target_depth
 
     msg = init_msg()
-    msg.axes[axes_dict['frontback']] = 0.4
+    msg.axes[axes_dict['frontback']] = 0.2
 
     if (time.time() - start_time) > 10:
         print("headed home motherfuckers")
@@ -162,7 +165,7 @@ def ramming_speed(boxes):
     else:
         msg.axes[axes_dict['vertical']] = hold_depth(target_depth) #replace with hold_depth later
 
-    pub.publish(msg)
+    return msg
 
 def surface(boxes):
     global current_state
@@ -177,7 +180,7 @@ def surface(boxes):
     else:
         rospy.spin() #wait for shutdown
 
-    pub.publish(msg)
+    return msg
 
     
 
@@ -188,7 +191,7 @@ def search_forward(boxes):
     global start_time
     global target_depth
     msg = init_msg()
-    msg.axes[axes_dict['frontback']] = .5
+    msg.axes[axes_dict['frontback']] = .2
     if get_box_of_class(boxes, current_target):
             current_state = track
     elif (time.time() - start_time) < 4: #move forward for 3 secs
@@ -275,7 +278,6 @@ def start(boxes):
     curr_msg = init_msg()
     curr_msg.axes[axes_dict['vertical']] = -1
     curr_msg.axes[axes_dict['frontback']] = 1
-    pub.publish(curr_msg)
     if time.time() > (start_time + 5):
         if get_box_of_class(boxes, class_dict['start_gate']):
             current_target = class_dict['start_gate']
@@ -290,21 +292,27 @@ def start(boxes):
 #set boxes
 def bbox_callback(msg):
     #get multidimensional list of boxes
-    boxes = []
-    num_boxes = int(msg.data[0])
-    for i in range (num_boxes):
-        boxes.append(msg.data[7 * i + 1: 7 * i + 7])
-    print(boxes)
+    #boxes = []
+    #num_boxes = int(msg.data[0])
+    #for i in range (num_boxes):
+    #    boxes.append(list(msg.data[7 * i + 1: 7 * i + 7]))
+    #    boxes[i][0] -= 1 #compensate for network weirdness
+    #print(boxes)
 
     #get function
-    output = current_state(boxes)
+    #output = current_state(boxes)
+    target_depth = get_depth()
+    while not rospy.is_shutdown():
+        msg = init_msg()
+        msg.axes[axes_dict['vertical']] = hold_depth(target_depth)
+        pub.publish(msg)
 
     #publish
-    pub.publish(output)
+    #.publish(output)
 
 
 #start execution here
-rospy.sleep(20)
+# rospy.sleep(20)
 current_state = start
 start_time = time.time()
 
