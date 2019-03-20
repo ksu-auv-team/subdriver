06458fc04b698f6471a86ad6d91b3f88580793be
BPod123 <BenPodrazhansky@gmail.com>
2019-03-20 18:51:42 -0400

    Changed dice names to bouy. Implemented bouy execute state.

---
+++ StateMachine/interact/interact_buoy.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from gbl import Box_Type as BOX
+from gbl import boxes
+from StateMachine.sub import *
+
+# define state interact_dice
+class interact_buoy(sub):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Clear_Of_Buoy'])
+
+    def execute(self, userdata):
+        self.init_state()
+        msg = self.init_joy_msg()
+        box = None
+        for(i in range(0,len(gbl.boxes)):
+            if(gbl.boxes[i][0]==BOX.buoy): # is a target
+                box_num = i
+
+
+        # Start motors
+        #msg.axis[self.axis_dict['frontback']] = 0.7
+
+        rospy.loginfo("Adjusting depth")
+        while(self.getCenter(boxes[box])[1]!=0):
+            if (self.getCenter(boxes[box])[1]>0):
+                msg.axis[self.axis_dict['vertical']] = 0.5
+            elif(self.getCenter(boxes[box])[1]<0):
+                msg.axis[self.axis_dict['vertical']] = -0.5
+            self.joy_pub.publish(msg)
+            rospy.sleep(gbl.sleep_time)
+        # Stabilize
+        msg.axis[self.axis_dict['vertical']] = self.depth_hold
+        self.joy_pub.publish(msg)
+
+        rospy.loginfo("Adjusting left to right")
+        while(self.getCenter(boxes[box])[0]!=0):
+            if (self.getCenter(boxes[box])[0]>0):
+                msg.axis[self.axis_dict['rotate']] = 0.5
+            elif(self.getCenter(boxes[box])[0]<0):
+                msg.axis[self.axis_dict['rotate']] = -0.5
+                self.joy_pub.publish(msg)
+                rospy.sleep(gbl.sleep_time)
+        # Stop rotating
+        msg.axis[self.axis_dict['rotate']] = 0
+        self.joy_pub.publish(msg)
+
+        rospy.loginfo("Moving forward")
+        while rospy.get_time() > (gbl.run_start_time + 15):
+            msg.axis[self.axis_dict['forward']]= 0.7
+            self.joy_pub.publish(msg)
+            rospy.sleep(gbl.sleep_time)
+
+        gbl.current_target = None
+
+
+
+        rospy.loginfo('Executing state INTERACT_BUOY')
+        return 'Clear_Of_Buoy'
--- StateMachine/search/search_front_buoy.py
+++ StateMachine/search/search_front_buoy.py
@@ -3,8 +3,9 @@
 from search_front import *
 
 # define state search_front_dice
-class search_front_dice(search_front):
-	pass
+class search_front_buoy(search_front):
+	def __init__(self):
+		smach.State.__init__(self, outcomes = 'buoy_found', 'buoy_hit')
 
 	def log(self):
 		rospy.loginfo('Executing state SEARCH_FRONT_DICE')
