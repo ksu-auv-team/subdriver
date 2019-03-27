f69ea9c25ef5311c2ee0f3d6fef630e8326c255f
auv <ksuauvteam@gmail.com>
2019-03-27 19:57:24 -0400

    changed architecture and added command line arguments

--- execute_withState.py
+++ execute_withState.py
@@ -1,14 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
-import StateMachine.StateMachines.BaseStateMachine as base
+import StateMachine.machines.BaseStateMachine as base
+import argparse
 
-def full_state_machine():
-	rospy.loginfo("Running 'full_state_machine' ...")
-	base.createStateMachine()
+parser = argparse.ArgumentParser(description="execute a state machine for the submarine")
+parser.add_argument('-m', '--machine', default = "full_state_machine", help="the name of the state machine to execute (default: %(default)s)")
+args = parser.parse_args()
+
+def hello_world():
+	print("Hi! I'm useless!")
+
+states = {
+	'full_state_machine': base.createStateMachine,
+	'test_machine': hello_world,
+	'this_is_filler': 2,
+	'to_show_the_concept': 3,
+}
 
 def main():
-	full_state_machine()
+	rospy.loginfo("Running {}".format(args.machine))
+	try:
+		states[args.machine]()
+	except KeyError:
+		rospy.logfatal("Error: state machine name not recognized")
 
 if __name__ == '__main__':
 	main()
