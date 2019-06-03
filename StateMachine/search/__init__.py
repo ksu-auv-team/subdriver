e5e4d16d2e42d6cfdb95bf8f93845f7be3fcb650
Tyler Gragg <robots@tylergragg.com>
2019-06-03 19:49:13 -0400

    removed automodinit dependency

--- StateMachine/search/__init__.py
+++ StateMachine/search/__init__.py
@@ -1,6 +1,1 @@
 __all__ = ['search_front', 'search_front_dice', 'search_front_gate', 'search_left', 'search_left_dice', 'search_left_gate', 'search_recenter', 'search_recenter_dice', 'search_recenter_gate', 'search_right', 'search_right_dice', 'search_right_gate']
-# Don't modify the line above, or this line!
-import automodinit
-automodinit.automodinit(__name__, __file__, globals())
-del automodinit
-# Anything else you want can go after here, it won't get modified.
