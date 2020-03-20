66d54aeb7c674bf2d0150f54ad186aded6bda4c6
nate sanchez <n.sanchez10@comcast.net>
2020-03-19 22:04:55 -0400

    i did my best

--- StateMachine/search/search_path.py
+++ StateMachine/search/search_path.py
@@ -1,27 +1,25 @@
-from StateMachine.edu import *
+from StateMachine.sub import *
 
-class Search_path(sub):
-  def_init_(self):
-    smach.State._init(self, outcomes= (['object_found', object_not found])
-    
-    def execute(self, userdata): 
-    self.__init__state()
-    gbl.state_heading = gbl.heading
-    msg = self._init_joy_msg()
-    msg.axes[const.AXES['frontback']] = 0.3
-    
+class Search_Path(Sub):
+    def __init__(self):
+        smach.State.__init__(self, outcomes= ['object_found', 'object_not found']
 
-start 
+    def execute(self, userdata):
+        self.init_state()
+        gbl.state_heading = gbl.heading
+        msg = self.init_joy_msg()
+        msg.axes[const.AXES['frontback']] = 0.3
 
-while(1):
-            self.joy_pub.publish(msg)
+        while(1):
+            self.publish_joy(msg)
 
             if self.get_box_of_class(gbl.detections_bottom, gbl.current_target):
                 if self.search_frames_seen <= 2:
                     self.search_frames_seen += 1
-                    
- else:
-                self.search_frames_seen = 0
-                return "object_not_found'
+                else:
+                    return "object_found"
 
-            rospy.sleep(const.SLEEP_TIME)    
+            else:
+                self.search_frames_seen = 0
+                return "object_not_found"
+            rospy.sleep(const.SLEEP_TIME)
